8efbc16aead1f27a1dd93289a0183d2e46c269e7
leesomin1204 <sm1157@naver.com>
2025-07-08 18:20:45 +0900

    07.08_2

--- src/member/containers/LoginContainer.js
+++ src/member/containers/LoginContainer.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState, useCallback } from 'react';
+import LoginForm from '../components/LoginForm';
 
 const LoginContainer = () => {
-    return <></>
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const onChange = useCallback((e) => {
+    setForm((form) => ({ ...form, [e.target.name]: e.target.value }));
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      const errors = {};
+      let hasErrors = false;
+      // 필수 항목 검증 S
+      const requiredFields = {
+        email: '이메일을 입력하세요.',
+        password: '비밀번호를 입력하세요.',
+      };
+
+      for (const [field, message] of Object.entries(requiredFields)) {
+        if (!form[field] || !form[field]?.trim()) {
+          errors[field] = message;
+          hasErrors = true;
+        }
+      }
+      // 필수 항목 검증 E
+      setErrors(errors);
+      if (hasErrors) {
+        return; // 검증 실패시 다음 로직 처리 X
+      }
+
+      // 로그인 처리..
+    },
+    [form],
+  );
+
+  return (
+    <LoginForm 
+        form={form} 
+        onChange={onChange} 
+        onSubmit={onSubmit} 
+        errors={errors}
+    />
+  );
 };
 
 export default React.memo(LoginContainer);
